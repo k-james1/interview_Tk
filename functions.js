b890b405c640e035868a96391c027f7598a22e68
k-james1 <keijames.travis.5z@stu.hosei.ac.jp>
2024-12-17 22:50:34 +0900

    add
    start

---
+++ functions.js
@@ -0,0 +1,28 @@
+const chapters = document.querySelectorAll('.chapter');
+
+chapters.forEach((chapter) => { //それぞれのチャプターについての
+  gsap.fromTo(chapter, {autoAlpha: 0, y: 50}, {
+    duration: .5,
+    autoAlpha: 1,
+    y: 0,
+    ease: 'power4.inOut',
+    scrollTrigger: {
+      trigger: chapter,
+      start: 'top 80%',
+      toggleActions: 'play none none none',
+      once: true,
+    }
+  });
+});
+
+gsap.from("body",{
+    scrollTrigger:{
+      trigger:"body",
+      start:"center center",
+      end:"bottom bottom",
+      scrub: true,
+    },
+    duration:0.1,
+    backgroundColor:"#3a7ca5",
+    ease:"none",
+  });
